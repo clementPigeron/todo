8a4ae861f732378a52a7398196019333b60ccac0
clementPigeron <clement.pigeron@gmail.com>
2025-05-08 17:55:23 +0200

    gros changements, début des textarea

--- script_todo.js
+++ script_todo.js
@@ -1,24 +1,44 @@
-var ol = document.querySelector('ol');
+const ol = document.querySelector('ol');
 var lineNumber = 0;
-var line
 var size = 0
-var button = document.getElementById("myButton");
+const button = document.getElementById("myButton");
+const rightCol = document.getElementById('content')
 
 button.addEventListener("click", () => {
     addLine();
+    addTextarea();
 })
 
-function addLine () {
+function addLine() {
     lineNumber++;
-    ol.insertAdjacentHTML("beforeend", `<li id="line${lineNumber}"><input placeholder="écrire" class="ecrire"> 
-        <input type="checkbox"> <button onclick="deleteLine(${lineNumber})">-</button></li>`);
+    ol.insertAdjacentHTML("beforeend",
+        `<li id="line${lineNumber}">
+            <input placeholder="écrire" class="ecrire"> 
+            <input type="checkbox"> 
+            <button onclick="deleteLine(${lineNumber});deleteTextarea(${lineNumber})"> - </button> 
+            <button onclick="displayTextarea(${lineNumber})"> > </button>
+        </li>`
+    );
     size++;
 }
 
-function deleteLine (currentli) {
-    if (typeof ondeleteline != "undefined") {ondeleteline()};
-    line = document.getElementById(`line${currentli}`);
+function deleteLine(currentli) {
+    if (typeof ondeleteline != "undefined") { ondeleteline() };
+    let line = document.getElementById(`line${currentli}`);
     console.log(currentli);
     line.remove();
     size--;
+}
+
+function addTextarea() {
+    rightCol.insertAdjacentHTML("beforeend", `<textarea id="text${lineNumber}" style="display:none;"></textarea>`);
+}
+
+function deleteTextarea(currentli) {
+    let area = document.getElementById(`text${currentli}`);
+    area.remove();
+}
+
+function displayTextarea(currentli) {
+
 }
